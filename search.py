1d04c78f7015870e55ed8492dad38913400882e8
TAKAAYU <ayumu.takahashi112@gmail.com>
2021-04-09 15:02:58 +0900

    完成課題

--- search.py
+++ search.py
@@ -1,17 +1,33 @@
-
+import pandas as pd
 ### 検索ツールサンプル
 ### これをベースに課題の内容を追記してください
 
 # 検索ソース
-source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+#source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+
 
 ### 検索ツール
 def search():
-    word =input("鬼滅の登場人物の名前を入力してください >>> ")
-    
-    ### ここに検索ロジックを書く
+    df = pd.read_csv('source.csv')
+    source = list(df["name"])
 
-    print("{}が見つかりした".format(word))
+    while True:
+
+        word =input("鬼滅の登場人物の名前を入力してください >>> ")
+
+        ### ここに検索ロジックを書く
+        if word in source:
+
+            print("{}が見つかりました".format(word))
+
+        else:
+            print("{}が見つかリませんでした".format(word))
+            print(word + 'を追加しました')
+            source.append(word)
+
+        df = pd.DataFrame(source, columns=["name"])
+        df.to_csv("source.csv")
+        print(df)
 
 if __name__ == "__main__":
     search()
